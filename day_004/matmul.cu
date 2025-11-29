bf015d73fe144eb20c981b56a834dd85b8f9c4ca
shubhamgore2468 <shubhamgore2468@gmail.com>
2025-11-29 10:44:51 -0800

    day 4

--- day_004/matmul.cu
+++ day_004/matmul.cu
@@ -8,12 +8,15 @@
 #define N 256 // Number of columns in B and C
 #define BLOCK_SIZE 32
 // Example 3x2 @ 2x4 = 3x4
+//         MxK  KxN    MxN
 // A = [[1 2],
 //      [3 4],
 //      [5 6]]
 
 // B = [[7  8  9 10],
 //.     [11 12 13 14]]
+
+// row major formula = A[row * n_cols + col]
 
 // C = A * B = [[1*7 + 2*11, 1*8 + 2*12, 1*9 + 2*13, 1*10 + 2*14],
 //              [3*7 + 4*11, 3*8 + 4*12, 3*9 + 4*13, 3*10 + 4*14],
@@ -24,22 +27,31 @@
 //      [101, 112, 123, 134]]
 
 
-__global__ void matmul(float*A, float*B, float*C, int M, int K, int N){
+__global__ void matmul(float*A, float*B, float*C, int m, int k, int n){
 
     int row = threadIdx.y + blockDim.y * blockIdx.y;
     int col = threadIdx.x + blockDim.x * blockIdx.x;
-    if (row >= M || col >= N) return;
+    if (row >= m || col >= n) return;
     float acc = 0.0f;
-    for (int i = 0; i<K; i++){
-        acc += A[row * K + i] * B[ i * N + col]; //row indexed by col - N
+    for (int i = 0; i<k; i++){
+        acc += A[row * k + i] * B[ i * n + col]; //row indexed by col - N
     }
-    C[row * N + col] = acc;
+    C[row * n + col] = acc;
 }
 
 void init_matrix(float *mat, int rows, int cols){
 
     for(int i=0; i<rows*cols; i++){
         mat[i] = (float)rand() / RAND_MAX;
+    }
+}
+
+void print_matrix(float *mat, int rows, int cols){
+    for(int i=0l i<rows; i++){
+        for(int j=0; j<cols; j++){
+            printf("%f ", mat[i*cols + j]);
+        }
+        printf("\n");
     }
 }
 
@@ -50,16 +62,16 @@ int main(){
     int size_B = K*N*sizeof(float);
     int size_C = M*N*sizeof(float);
 
-    h_a = (float)malloc(size_A);
-    h_b = (float)malloc(size_B);
-    h_c = (float)malloc(size_C);
+    h_a = (float*)malloc(size_A);
+    h_b = (float*)malloc(size_B);
+    h_c = (float*)malloc(size_C);
 
     init_matrix(h_a, M, K);
-    init_matrix(h_b, N, K);
+    init_matrix(h_b, K, N);
 
     cudaMalloc(&d_a, size_A);
     cudaMalloc(&d_b, size_B);
-    cudaMalloc(&d_C, size_C);
+    cudaMalloc(&d_c, size_C);
 
     cudaMemcpy(d_a, h_a, size_A, cudaMemcpyHostToDevice);
     cudaMemcpy(d_b, h_b, size_B, cudaMemcpyHostToDevice);
@@ -72,6 +84,11 @@ int main(){
 
     matmul<<<gridDim, blockDim>>>(d_a, d_b, d_c, M, K, N);
     cudaDeviceSynchronize();
+
+    cudaMemcpy(h_c, d_c, size_C, cudaMemcpyDeviceToHost);
+
+    print_matrix(h_c, M, N);
+
     free(h_a);
     free(h_b);
     free(h_c);
